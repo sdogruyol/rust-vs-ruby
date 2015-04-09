bc2919728f0d6137491466d39b59a0ec036f3e3d
Sdogruyol <dogruyolserdar@gmail.com>
2015-04-09 22:15:57 +0300

    Added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust vs Ruby: Building an API
+
+This is the repository for Rust vs Ruby: Building an API blog post.
--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -4,12 +4,10 @@ require 'json'
 # To launch just type: 'rackup' in your console
 Cuba.define do
   on get do
-
     on root do
       data = { first_name: 'Serdar', last_name: 'Dogruyol' }
       res.headers["Content-Type"] = "application/json; charset=utf-8"
       res.write data.to_json
     end
-
   end
 end
