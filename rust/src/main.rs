91cd851fd581c278e6ec812ec60efb036c47c14a
Sdogruyol <dogruyolserdar@gmail.com>
2015-04-09 22:00:38 +0300

    Rust example working

---
+++ rust/src/main.rs
@@ -0,0 +1,37 @@
+#[macro_use] extern crate nickel_macros;
+extern crate rustc_serialize;
+extern crate nickel;
+
+use std::collections::BTreeMap;
+use nickel::{Nickel,HttpRouter};
+use rustc_serialize::json::{Json, ToJson};
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+          let person = Person{
+            first_name: "Serdar".to_string(),
+            last_name: "Dogruyol".to_string()};
+          person.to_json()
+        }
+    });
+
+    server.listen("127.0.0.1:6767");
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Person {
+  first_name: String,
+  last_name: String,
+}
+
+impl ToJson for Person {
+    fn to_json(&self) -> Json {
+        let mut map = BTreeMap::new();
+        map.insert("first_name".to_string(), self.first_name.to_json());
+        map.insert("last_name".to_string(), self.last_name.to_json());
+        Json::Object(map)
+    }
+}
